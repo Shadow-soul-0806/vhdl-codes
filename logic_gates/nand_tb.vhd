86c57b2a6d38dd3454ae27fcf38953c381b4bf3d
Shadow-soul-0806 <ngvasava2000@gmail.com>
2026-04-14 00:35:50 +0530

    Add files via upload

---
+++ logic_gates/nand_tb.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity nand_tb is 
+end nand_tb;
+
+architecture simulation of nand_tb is 
+
+	signal a_tb ,b_tb : std_logic := '0';
+	signal y_tb : std_logic;
+begin
+
+	dut : entity work.nand_gate
+	port map (
+	a => a_tb ,
+	b => b_tb ,
+	y => y_tb 
+	);
+
+	process
+
+	begin 
+
+	for i in 0 to 3 loop
+
+
+	a_tb <= std_logic(to_unsigned(i,2)(1));
+	b_tb <= std_logic(to_unsigned(i,2)(0));
+
+	wait for 20 ps;
+
+	end loop ;
+
+	wait ;
+
+	end process;
+
+
+end simulation ;
